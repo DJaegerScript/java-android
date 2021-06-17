992662a8f9c4b3d4c35b42969f51e15124e5ff69
AdjieDjaeger <spiritjaeger@gmail.com>
2021-06-18 01:39:02 +0700

    feat(): Created Hello World Project

--- HelloWorld/app/src/main/java/com/codewithdjaeger/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/codewithdjaeger/helloworld/MainActivity.java
@@ -3,12 +3,31 @@ package com.codewithdjaeger.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mainText;
+    private EditText yourText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mainText = findViewById(R.id.main_text);
+        this.yourText = findViewById(R.id.your_text);
+
+        Button textChanger = findViewById(R.id.text_changer);
+
+        textChanger.setOnClickListener(v -> this.changeText());
+    }
+
+    private void changeText() {
+        String yourTextValue = this.yourText.getText().toString();
+        this.mainText.setText(yourTextValue);
+        this.yourText.getText().clear();
     }
 }
